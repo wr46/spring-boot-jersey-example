d154e41884e9bc90745e52cfc143c05223c7349c
Wilson Rodrigues <w.rodrigues@xgeeks.io>
2021-10-29 23:18:01 +0100

    feat(project-template): create project template

---
+++ src/main/java/com/github/wr46/springbootjerseyexample/SpringBootJerseyExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.wr46.springbootjerseyexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJerseyExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJerseyExampleApplication.class, args);
+	}
+
+}
